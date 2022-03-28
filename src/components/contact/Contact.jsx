5d93c53fc26ac70cef0ecb20244f8d2ead9a73ec
Alexandre Ciorascu <alexandre.ciorascu@u-psud.fr>
2022-03-28 17:53:22 +0200

    new pic

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,18 +12,18 @@ const Contact = () => {
         <p>
         <div className='site__contact-linkedin'>
           <img src={linkedin} alt="linkedin"/>
-          ‣ LinkedIn :  <a href="https://www.linkedin.com/in/alexandre-ciorascu/">Alexandre Ciorascu</a> 
+          ‣ LinkedIn : &nbsp; <a href="https://www.linkedin.com/in/alexandre-ciorascu/" target="_blank" rel='noreferrer'>Alexandre Ciorascu</a> 
         </div>
           <br></br>
           <div className='site__contact-git'>
             <img src={git} alt="git"/>
-            ‣ GitHub : <a href="https://github.com/sukaizer">sukaizer</a>
+            ‣ GitHub : &nbsp; <a href="https://github.com/sukaizer" target="_blank" rel='noreferrer'>sukaizer</a>
         </div>
         
           <br></br>
           <div className='site__contact-mail'>
             <img src={mail} alt="mail"/>
-          ‣ Email : <a href="mailto:alexandre.ciorascu@universite-paris-saclay.fr">alexandre.ciorascu@universite-paris-saclay.fr</a>  
+          ‣ Email : &nbsp; <a href="mailto:alexandre.ciorascu@universite-paris-saclay.fr" target="_blank" rel='noreferrer'>alexandre.ciorascu@universite-paris-saclay.fr</a>  
         </div>
           
       </p>
