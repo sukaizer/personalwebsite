1a27b80b0d5c4088493bfa9c0d8fc4e9705734fb
Alexandre Ciorascu <alexandre.ciorascu@yahoo.fr>
2023-07-24 13:49:32 +0200

    Update Gallery.jsx

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -15,7 +15,35 @@ const images = [
     original: 'https://picsum.photos/id/1019/1000/600/',
     thumbnail: 'https://picsum.photos/id/1019/250/150/',
   },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    embedUrl: 'https://www.youtube.com/embed/4pSzhZ76GdM?autoplay=1&showinfo=0',
+    description: 'Render custom slides (such as videos)',
+    renderItem: _renderVideo.bind(this)
+  },
+  
 ];
+
+function _renderVideo(item) {
+  return (
+    <div>
+      {
+        <div className='video-wrapper'>
+            <iframe
+       
+              src={item.embedUrl}
+              frameBorder='0'
+              allowFullScreen
+              onload="this.style.height=(this.contentWindow.document.body.scrollHeight+20)+'px';"
+            >
+            </iframe>
+        </div>
+      }
+    </div>
+  );
+}
+
 
 const Gallery = ({active}) => {
   
