abe5c47c7cff94d611db66636fb7495f285ecff1
Alexandre Ciorascu <alexandre.ciorascu@u-psud.fr>
2023-12-22 10:56:56 +0100

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,12 +2,25 @@ import React from 'react'
 import "./about.css"
 
 const About = () => {
+  const birthDate = new Date(2001, 2, 1); // March is represented by 2 in JavaScript Date object
+  const currentDate = new Date();
+  let age = currentDate.getFullYear() - birthDate.getFullYear();
+  
+  // Check if the current date is before the birth date in the current year
+  if (
+    currentDate.getMonth() < birthDate.getMonth() ||
+    (currentDate.getMonth() === birthDate.getMonth() &&
+      currentDate.getDate() < birthDate.getDate())
+  ) {
+    age--;
+  }
+  
   return (
     <div className='site__about'>
       <h3> about <span className='me'>me</span></h3>
       <div className='site__about-text'>
         <p>
-          ‣ 22 years old. HCI master student at Université Paris-Saclay. 
+          ‣ {age} years old. HCI master student at Université Paris-Saclay. 
           <br />
           <br />
           ‣ <b>maths</b> and <b>computer science</b> double degree bachelor. 👨‍🎓
